9adb8388d1f6eeb6878f739baa7f05e5996566be
bloomber <fortunati.mario@gmail.com>
2018-09-28 10:56:25 +0200

    Kata Mario
    Kata original

---
+++ csharp/Rule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public enum TypeOfRule
+    {
+        Quality,
+        SellIn,
+        BornMaxQuality,
+        BornMinQuality,
+        OutOfDate
+    }
+    public class Rule
+    {
+
+        public string NameOfProduct { get; set; }
+        public char Operator { get; set; }
+        public int SellInValue { get; set; }
+
+        public int QualityValue { get; set; }
+
+        public TypeOfRule TypeOfRuleValue { get; set; }
+    }
+}
